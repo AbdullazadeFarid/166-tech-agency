cd3b0ed7c89b3513720ddfeb898c9bbba934ccf3
AbdullazadeFarid <abdullazadefarid@mail.ru>
2024-09-11 20:48:56 +0300

    ,

--- src/slider/slider.jsx
+++ src/slider/slider.jsx
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 import user from '../assets/image/user.png'
-import ulduz from '../assets/icon/stars.png'
+import ulduz from '../assets/icon/Star.svg'
 
 import 'swiper/css';
 import "./slider.css"
